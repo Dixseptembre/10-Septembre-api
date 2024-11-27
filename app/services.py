5e5ac295262f6ac5a83b15b93413e666d0044b54
ACHRAF FAYTOUT <achraffaytout1@gmail.com>
2024-11-27 13:27:34 +0100

    Adding "patronal" charges

--- app/services.py
+++ app/services.py
@@ -9,15 +9,16 @@ def process_file(file):
 
     # Extraire les données
     employees = df.iloc[0].dropna().unique().tolist()
-    result = []
+    result = {"employees": [], "libelle_patronal": []}  # Ajouter libelle_patronal au niveau global
 
     for idx, employee in enumerate(employees):
-        employee_data = {"name": employee, "infos": []}  # Initialize infos as a ls
+        employee_data = {"name": employee, "infos": []}
+
         for _, row in df.iloc[2:].iterrows():
-            libelle = row.iloc[1]
-            base_s = clean_value(row.iloc[2 + idx * 3])
-            salarial = clean_value(row.iloc[3 + idx * 3])
-            patronal = clean_value(row.iloc[4 + idx * 3])
+            libelle = row.iloc[1]  # Libellé
+            base_s = clean_value(row.iloc[2 + idx * 3])  # Base S.
+            salarial = clean_value(row.iloc[3 + idx * 3])  # Salarial
+            patronal = clean_value(row.iloc[4 + idx * 3])  # Patronal
 
             # Append (libelle and related infos)
             employee_data["infos"].append({
@@ -26,6 +27,9 @@ def process_file(file):
                 "Salarial": salarial,
                 "Patronal": patronal
             })
-        result.append(employee_data)
-    result.pop()
+            # Ajouter le libellé à la liste globale si "patronal" est non nul
+            if patronal != 0 and libelle not in result["libelle_patronal"] and _ < df.index[df.iloc[:, 1] == 'Total des retenues déductibles'].tolist()[0]:
+                result["libelle_patronal"].append(libelle)
+        result["employees"].append(employee_data)
+    result["employees"].pop()
     return result
