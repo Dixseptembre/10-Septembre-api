179579e9f3c9a995f321302f29b79726101bd524
ACHRAF FAYTOUT <achraffaytout1@gmail.com>
2024-12-04 16:21:01 +0100

    Adding A type

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, request, jsonify, Flask
 import json
-from app.services import process_file
+from app.services import process_file_cbis, process_file_A
 
 main = Blueprint('main', __name__)
 
-@main.route('/process', methods=['POST'])
-def process_endpoint():
+@main.route('/process_cbis', methods=['POST'])
+def process_cbis_endpoint():
     try:
         if 'file' not in request.files:
             return jsonify({"success": False, "error": "No file uploaded"}), 400
@@ -15,7 +15,29 @@ def process_endpoint():
             return jsonify({"success": False, "error": "Invalid file format. Only .xlsx allowed"}), 400
 
         # Traitement du fichier
-        result = process_file(file)
+        result = process_file_cbis(file)
+        return Flask.response_class(
+            response=json.dumps({"success": True, "data": result}, ensure_ascii=False, indent=4),
+            status=200,
+            mimetype='application/json'
+        )
+
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)}), 500
+    
+
+@main.route('/process_A', methods=['POST'])
+def process_A_endpoint():
+    try:
+        if 'file' not in request.files:
+            return jsonify({"success": False, "error": "No file uploaded"}), 400
+
+        file = request.files['file']
+        if not file.filename.endswith('.xlsx') and not file.filename.endswith('.xlsm'):
+            return jsonify({"success": False, "error": "Invalid file format. Only .xlsx / .xlsm allowed"}), 400
+
+        # Traitement du fichier
+        result = process_file_A(file)
         return Flask.response_class(
             response=json.dumps({"success": True, "data": result}, ensure_ascii=False, indent=4),
             status=200,
--- app/services.py
+++ app/services.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from app.utils import clean_value
 
-def process_file(file):
+def process_file_cbis(file):
     # Charger le fichier
-    df = pd.read_excel(file, header=[2, 1])  # Lire avec un en-tête multi-niveau
+    df = pd.read_excel(file, sheet_name=0, header=[2, 1])  # Lire avec un en-tête multi-niveau
     if df.empty:
         raise ValueError("The file is empty or invalid format")
 
@@ -32,5 +32,58 @@ def process_file(file):
             if patronal != 0 and libelle not in result["libelle_patronal"] and _ < df.index[df.iloc[:, 1] == 'Total des retenues déductibles'].tolist()[0]:
                 result["libelle_patronal"].append(libelle)
         result["employees"].append(employee_data)
+
+    return result
+
+def process_file_A(file):
+    # Load with appropriate header levels
+    df = pd.read_excel(file, sheet_name=0, header=[2, 1])  # Assuming a simple single-level header
+    if df.empty:
+        raise ValueError("The file is empty or invalid format")
+    df = df.iloc[1:]
+    df.columns = df.columns.get_level_values(1)
+    # Init the structure
+    result = {"employees": [], "libelle_patronal": []}
+    
+    # Loop through the rows and process each employee's data
+    employee_data = {}
+    
+    for index, row in df.iterrows():
+        # Detect new employee based on the first column
+        if pd.notna(row.iloc[0]) and isinstance(row.iloc[0], (int, float)):
+            # If there's an existing employee, add them to the result
+            if employee_data:
+                result["employees"].append(employee_data)
+                employee_data = {}  # Reset for the new employee
+            
+            # Start a new employee record
+            employee_name = row.iloc[1]  # Employee name is in the second column
+            employee_data = {"name": employee_name, "infos": []}
+            continue # Skip because New
+
+        # Data for the current employee
+        if employee_data:
+            row_name = row.iloc[0]  # Row name is in the first column
+            base_s = clean_value(row.iloc[df.columns.get_loc('Base ou Nombre')])  # Base S.
+            salarial = clean_value(row.iloc[df.columns.get_loc('Part Salariale Gains')])  # Salarial
+            patronal = clean_value(row.iloc[df.columns.get_loc('Part Patronale Retenues')])  # Patronal
+
+            # Append the row data to the current employee's infos
+            row_info = {
+                "Libellé": row_name,
+                "Base S.": base_s,
+                "Salarial": salarial,
+                "Patronal": patronal
+            }
+            employee_data["infos"].append(row_info)
+
+            # Add libellé
+            if patronal != 0 and row_name not in result["libelle_patronal"] \
+                and not row_name[0].isdigit() \
+                    and index < df.index[df.iloc[:, 0] == '50_COTIS_DEDUCTIBLE'].tolist()[0]:
+                result["libelle_patronal"].append(row_name)
+    
+    # Add the final employee
+    result["employees"].append(employee_data)
 
     return result
