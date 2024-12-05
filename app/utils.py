178e7ace2ba1e4b8478cabb3cb6219145f2f178e
ACHRAF FAYTOUT <achraffaytout1@gmail.com>
2024-12-05 15:07:54 +0100

    Add new utils for Cbis

--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,25 @@ def clean_value(value):
     float others
     """
     return float(value) if value==value else 0
+
+def cbis_normalize(df):
+    """
+    Try to increase population of C bis
+    1) Normal
+    2) With no header
+    """
+    if df.iloc[0].isna().all() :
+        df = df.rename(columns=df.iloc[1])
+        df = df.drop([0, 1])
+        df = df.reset_index(drop=True)
+    df = df.drop(0)
+    return df
+
+def list_employees(df):
+    """
+    Drop Nan values from the header
+    & start with unnamed
+    """
+    employees = df.columns.dropna().tolist()
+    employees = [col.strip() for col in employees if not col.strip().startswith("Unnamed")]
+    return employees
